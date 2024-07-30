c7049ec4740ccf078af9a4577117b1e6bd10bd87
DevvratSahu07 <devvratsahu223@gmail.com>
2024-07-31 00:57:49 +0530

    shift mongoDb to online server

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost:27017/hotels';
+require('dotenv').config();
+
+// Define the MongoDB connection URL
+// const mongoURL = process.env.MONGODB_RUL_LOCAL; // local URL
+const mongoURL = process.env.MONGODB_URL; // Online (MongoDB Atlas Url)
 
 // Setup mongodb connection
 mongoose.connect(mongoURL,{
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const db = require('./db')
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -20,7 +23,7 @@ const { model } = require('mongoose');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoute);
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log("Listening at port 3000");
 })
 
